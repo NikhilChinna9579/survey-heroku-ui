afcac3b1649e1fb68245cc319d4f939313d482aa
NikhilChinna9579 <Nikilchinna9579@gmail.com>
2022-07-13 16:07:50 +0530

    fourth Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,7 @@ import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
 import { AdminViewfeedPageComponent } from './admin-viewfeed-page/admin-viewfeed-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { Feed2Component } from './feed2/feed2.component';
-import { AgGridModule } from 'ag-grid-angular'; 
+import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DisplayDialogComponent } from './display-dialog/display-dialog.component';
@@ -21,7 +20,6 @@ import { DefaultService } from './Services/default.service';
     FeedbackPageComponent,
     AdminViewfeedPageComponent,
     MainPageComponent,
-    Feed2Component,
     LoginPageComponent,
     DisplayDialogComponent
   ],
